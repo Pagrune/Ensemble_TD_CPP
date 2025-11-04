94c84a4088825de262fc4a7ef5ce027810f04775
Pagrune <pauline.grunewald2@gmail.com>
2025-11-04 10:27:01 +0100

    récupération arguments

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
     string name = "Hello World!";
     cout << name << endl;
+
+    // récupérer arguments de la ligne de commande
+    for (int i = 0; i < argc; i++) {
+        cout << "Argument " << i << ": " << argv[i] << endl;
+    }
+    
     cout << "Hello World!";
     return 0;
 }
