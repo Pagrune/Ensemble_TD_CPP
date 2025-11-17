5c6cabb368d4c0e3d0b4937ccedd2c16be698cd6
Pagrune <pauline.grunewald2@gmail.com>
2025-11-17 14:45:49 +0100

    ajout mesure compléxité en temps avec chrono

--- Map.cpp
+++ Map.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 void countFrequenciesBrutForce(const vector<int>& numbers){
        set<int> printed;  // garde les nombres déjà affichés
@@ -36,14 +38,24 @@ map<int, int> countFrequenciesOptimal(const vector<int>& numbers){
 int main(){
     vector<int> numbers = {1, 2, 3, 2, 4, 1, 5, 5, 6};
 
-    cout << "Frequeny (Brute Force):" << endl;
+     // Mesurer le temps pour la méthode brute-force
+    auto startBF = high_resolution_clock::now();
+    cout << "Frequency (Brute Force):" << endl;
     countFrequenciesBrutForce(numbers);
+    auto endBF = high_resolution_clock::now();
+    auto durationBF = duration_cast<microseconds>(endBF - startBF);
+    cout << "Brute Force duration: " << durationBF.count() << " microseconds" << endl << endl;
 
+    // Mesurer le temps pour la méthode optimale
+    auto startOpt = high_resolution_clock::now();
     cout << "Frequency (Optimal):" << endl;
     map<int, int> FrequencyMapOptimal = countFrequenciesOptimal(numbers);
     for(const auto& entry : FrequencyMapOptimal){
-        cout << entry.first << ": " << entry.second << "times" << endl;
+        cout << entry.first << ": " << entry.second << " times" << endl;
     }
+    auto endOpt = high_resolution_clock::now();
+    auto durationOpt = duration_cast<microseconds>(endOpt - startOpt);
+    cout << "Optimal duration: " << durationOpt.count() << " microseconds" << endl;
 
     return 0;
 }
